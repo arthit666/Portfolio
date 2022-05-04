7e28078b54db8ea1510dc06f024e29b8ef59cb92
SPT <arthit0ak@gmail.com>
2022-05-04 13:51:35 +0700

    commit

--- src/data.js
+++ src/data.js
@@ -1,37 +1,43 @@
+import vibration from './img/vibration.jpg'
+import blog from './img/blog.png'
+import nike from './img/nike.png'
+import travel from './img/travel.jpg'
+import codepen from './img/codepen.png'
+import js from './img/js.png'
 export const products = [
   {
     id: 1,
-    img: "https://www.img.in.th/images/ae9eca869daa2c8bc73b45b6cd5f91be.jpg",
+    img: vibration,
     link: "https://arthit-vibration-iot.herokuapp.com/",
     name: "Vibration-IOT"
   },
   {
     id: 2,
-    img: "https://www.img.in.th/images/215e3b71086fd34a493990fff6757e32.png",
+    img: blog,
     link: "https://arthit-blog-app.herokuapp.com/",
     name: "Blog-App"
   },
   {
     id: 3,
-    img: "https://www.img.in.th/images/d34e00792387cc1f6b61654a73822d43.png",
+    img: nike,
     link: "https://arthit666.github.io/E-commerce-Website-Nike/",
     name: "E-commerce-Nike"
   },
   {
     id: 4,
-    img: "https://www.img.in.th/images/0f9ab3949e974df07de52ac79c68f078.jpg",
+    img: travel,
     link: "https://travel-arthit.netlify.app/",
     name: "Travel"
   },
   {
     id: 5,
-    img: "https://www.img.in.th/images/ab071b03a5fa513bf48d5919634e1600.png",
+    img: codepen,
     link: "https://codepen.io/arthit666",
     name: "My-Codeped"
   },
   {
     id: 6,
-    img: "https://www.img.in.th/images/a2c7cb5b30fec6d94d1985b60d0302ed.png",
+    img: js,
     link: "https://github.com/Arthit666/JavaScript-21-Days-Challenge",
     name: "Js-21-Challenge"
   },
