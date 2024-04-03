92ab43ba786c2d3015f0178a7aa38b3ee7bf8909
Arthit666 <arthit0ak@gmail.com>
2024-04-03 18:37:48 +0700

    re write for new job :0

--- src/components/projectlList/ProjectList.jsx
+++ src/components/projectlList/ProjectList.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import "./projectList.css";
 import Project from "../project/Project";
-import { products } from "../../data";
+import { my_project, previous_project } from "../../data";
 const ProjectList = () => {
   return (
     <div className="pl">
       <div className="pl-texts">
-        <h1 className="pl-title">My Project ...</h1>
+        <h1 className="pl-title">Previous Project ...</h1>
       </div>
       <div className="pl-list">
-        {products.map((item) => (
+        {previous_project.map((item) => (
+          <Project key={item.id} img={item.img} link={item.link} name={item.name} />
+        ))}
+      </div>
+      <div className="pl-texts">
+        <h1 className="pl-title">Example Project ...</h1>
+      </div>
+      <div className="pl-list">
+        {my_project.map((item) => (
           <Project key={item.id} img={item.img} link={item.link} name={item.name} />
         ))}
       </div>
--- src/data.js
+++ src/data.js
@@ -1,45 +1,56 @@
-import vibration from './img/vibration.jpg'
+// import vibration from './img/vibration.jpg'
 import blog from './img/blog.png'
-import nike from './img/nike.png'
-import travel from './img/travel.jpg'
+// import travel from './img/travel.jpg'
 import codepen from './img/codepen.png'
-import js from './img/js.jpg'
-export const products = [
+import vibration_full from './img/vibration_full.png'
+import banking from './img/banking_api.png'
+import site_around_fm from './img/site_around_fm.png'
+// import js from './img/js.jpg'
+export const my_project = [
   {
     id: 1,
-    img: vibration,
-    link: "https://arthit-vibration-iot.herokuapp.com/",
-    name: "Vibration-IOT"
+    img: banking,
+    link: "https://github.com/arthit666/go_banking",
+    name: "Banking_API"
   },
   {
     id: 2,
-    img: js,
+    img: vibration_full,
     link: "https://github.com/Arthit666/Flutter_vibration_demo",
-    name: "Flutter-Vibration-IOT"
+    name: "Vibration-IOT"
   },
+  // {
+  //   id: 3,
+  //   img: js,
+  //   link: "https://github.com/Arthit666/Flutter_vibration_demo",
+  //   name: "Flutter-Vibration-IOT"
+  // },
   {
-    id: 3,
+    id: 4,
     img: blog,
     link: "https://github.com/arthit666/MernStack-BlogApp",
     name: "Blog-App"
   },
   
-  {
-    id: 4,
-    img: nike,
-    link: "https://arthit666.github.io/E-commerce-Website-Nike/",
-    name: "E-commerce-Nike"
-  },
-  {
-    id: 5,
-    img: travel,
-    link: "https://travel-arthit.netlify.app/",
-    name: "Travel"
-  },
+  // {
+  //   id: 5,
+  //   img: travel,
+  //   link: "https://travel-arthit.netlify.app/",
+  //   name: "Travel"
+  // },
   {
     id: 6,
     img: codepen,
     link: "https://codepen.io/arthit666",
     name: "My-Codeped"
+  },
+];
+
+export const previous_project = [
+  {
+    id: 11,
+    img: site_around_fm,
+    link: "https://play.google.com/store/apps/details?id=com.sitearound.fm&hl=th&gl=US",
+    name: "Site Around"
   },
 ];
